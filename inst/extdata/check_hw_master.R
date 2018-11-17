bb9c7dafc2d0ef23a93cccc7868ea57d7375f796
Tal Galili <tal.galili@gmail.com>
2018-11-17 09:32:10 +0200

    added check_hw_master.R

---
+++ inst/extdata/check_hw_master.R
@@ -0,0 +1,8 @@
+
+library(homework)
+demo_base_dir <- file.path(system.file(package = "homework"), "extdata")
+demo_base_dir
+check_hw("HW01", demo_base_dir)
+
+# etc.
+check_hw("HW02", demo_base_dir)
