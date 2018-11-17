bc6bac07a7b2156e7094cfbbeb548e1804fd8615
Tal Galili <tal.galili@gmail.com>
2018-11-17 09:56:41 +0200

    in real life there are many warnings

--- inst/extdata/check_hw_master.R
+++ inst/extdata/check_hw_master.R
@@ -3,6 +3,9 @@ library(homework)
 demo_base_dir <- file.path(system.file(package = "homework"), "extdata")
 demo_base_dir
 check_hw("HW01", demo_base_dir)
+# run the following to see any warnings within R:
+warnings()
+
 
 # etc.
 check_hw("HW02", demo_base_dir)
